c359d0999831a8043f0240e07dd081b3cca1328b
Yousuf Darr <yousuf.darr@gmail.com>
2025-11-05 17:43:25 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Authenticated, Unauthenticated } from "convex/react";
+import { SignInButton, UserButton, SignOutButton } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
+export default function Home() {
+  return (
+    <>
+      <Authenticated>
+        <UserButton />
+        <SignOutButton />
+        <Content />
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  );
+}
+
+function Content() {
+  const messages = useQuery(api.messages.getForCurrentUser);
+  return <div>Authenticated content: {messages?.length}</div>;
+}
